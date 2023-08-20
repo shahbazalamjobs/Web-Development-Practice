e35c2fbb84887d7a0cb1523f87bc510f8acf8322
Shahbaz Alam <shahbazalamjobs@gmail.com>
2023-08-20 20:37:05 +0530

    Update index.js

--- 05-Nodejs/01-w3-school/02-Create own module/index.js
+++ 05-Nodejs/01-w3-school/02-Create own module/index.js
@@ -1,5 +1,23 @@
+// 1. This gives output, current date and time in console 
 // Using ES6 modules
 import { myDateTime } from './myFirstModule.js';
 
 const currentDateTime = myDateTime();
 console.log("\n", currentDateTime);
+
+
+
+// 2. Server gives current date time on request of client 
+
+// Get the current date and time 
+const currentDateTime = myDateTime();
+console.log("\n", currentDateTime);
+
+// Create an HTTP server
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write("The date and time are currently: " + currentDateTime);
+    res.end();
+}).listen(8080);
+
+// O/p: The date and time are currently: Sun Aug 20 2023 20:31:21 GMT+0530 (India Standard Time)
