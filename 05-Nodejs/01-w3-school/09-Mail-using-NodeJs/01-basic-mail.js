fe121197207550d8d351a538d23ae47c2ef9562d
Shahbaz Alam <shahbazalamjobs@gmail.com>
2023-08-22 17:19:33 +0530

    Update 01-basic-mail.js

--- 05-Nodejs/01-w3-school/09-Mail-using-NodeJs/01-basic-mail.js
+++ 05-Nodejs/01-w3-school/09-Mail-using-NodeJs/01-basic-mail.js
@@ -12,7 +12,7 @@ const transporter = nodemailer.createTransport({
 // Setup email data
 const mailOptions = {
     from: 'your_email@gmail.com', // Sender's email address
-    to: 'recipient@example.com', // Recipient's email address, can add more using ','
+    to: 'recipient@example.com', // Recipient's email address, can add more like this 'a1@mail.in, a2@mail.in, a3@mail.in'
     subject: 'Hello from Node.js!', // Subject line
     text: 'This is a test email sent from Node.js using nodemailer.', // Plain text body
     html: '<p>This is a <b>test</b> email sent from <i>Node.js</i> using <strong>nodemailer</strong>.</p>' // HTML body
